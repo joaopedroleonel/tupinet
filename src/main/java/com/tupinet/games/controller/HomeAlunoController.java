fcfeb46a38fcd1b406d6a5ef2e95370e8cd65606
davieto <dchagas0001@gmail.com>
2025-06-03 02:36:18 -0300

    Feat: Adicionado Front da home e selceção da parte de alunos

---
+++ src/main/java/com/tupinet/games/controller/HomeAlunoController.java
@@ -0,0 +1,14 @@
+package com.tupinet.games.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeAlunoController {
+    @GetMapping("/codAluno")
+    public String homeAluno(){
+        return "codigoAluno.html";
+    }
+}
